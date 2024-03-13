bdf6e1b998b83475e6d60d082f11dd5fbbee221f
Apostrophel <sjurbarndon@proton.me>
2024-03-13 14:53:08 +0100

    fixed blog

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,3 +10,19 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+
+function showOverlay() {
+    var overlay = document.getElementById('overlay');
+    overlay.style.display = 'block'; // Display the overlay
+
+
+        // Remove the fade-out class if it exists
+        overlay.classList.remove('fade-out');
+    
+        // Trigger reflow before adding the class again
+        void overlay.offsetWidth;
+        
+        // Reapply the fade-out class
+        overlay.classList.add('fade-out');
+  }
+
