6f3cc1a09dcd2e550a64a0bd56e1c7a3d349d81c
Apostrophel <sjurbarndon@proton.me>
2024-03-14 13:52:31 +0100

    thesis update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,5 @@
-// add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
-
-window.onscroll = function() {
-    var top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
-}
+// This scripts had various functions to load common sections, showing overlays and do reactive changes to the site.
+// © 2024 Sjur Barndon <sjurbarndon@proton.me>
 
 function showOverlay() {
     var overlay = document.getElementById('overlay');
@@ -33,4 +23,27 @@ function loadFooter() {
             document.getElementById('footerSection').innerHTML = html;
         })
         .catch(error => console.error('Error fetching included content:', error));
+
+    fetch('/assets/common/navigation.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('myNavigationBar').innerHTML = html;
+            // add class navbarDark on navbar scroll
+            const header = document.querySelector('.navbar');
+
+            window.onscroll = function() {
+                var top = window.scrollY;
+                if(top >=100) {
+                    header.classList.add('navbarDark');
+                }
+                else {
+                    header.classList.remove('navbarDark');
+                }
+            }
+        })
+        .catch(error => console.error('Error fetching included content:', error));
+
+           
 }
+
+
