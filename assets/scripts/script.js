e63ab68ae88fef385c98a5f081dfc84a76478277
Apostrophel <sjurbarndon@proton.me>
2024-03-14 22:28:39 +0100

    fixes photo gallery new version

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -44,4 +44,9 @@ function loadFooter() {
         .catch(error => console.error('Error fetching included content:', error));
 }
 
+window.addEventListener("load", () => {
+    for (let i of document.querySelectorAll(".gallery img")) {
+      i.onclick = () => i.classList.toggle("full");
+    }
+  });
 
